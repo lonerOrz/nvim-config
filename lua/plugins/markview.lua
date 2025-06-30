7aa1825a9284f0cd0d324bb58b345b1961613ca7
loner <2788892716@qq.com>
2025-07-01 01:33:39 +0800

    markview

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,10 +1,18 @@
 return {
   "OXY2DEV/markview.nvim",
-  lazy = false, -- Recommended
-  -- ft = "markdown" -- If you decide to lazy-load anyway
+  lazy = false,   -- Recommended
+  ft = "markdown", -- If you decide to lazy-load anyway
 
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "nvim-tree/nvim-web-devicons",
   },
+
+  config = function()
+    require("markview").setup({
+      experimental = {
+        check_rtp_message = false, -- 关闭加载顺序提示信息
+      },
+    })
+  end,
 }
