eb8b23db06d3a0d6bf2266a3835d7c784db83c6b
loner <2788892716@qq.com>
2025-07-01 01:36:09 +0800

    upgrade && mason/bin

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+-- Add mason bin path to PATH
+vim.env.PATH = vim.env.PATH .. ":" .. vim.fn.stdpath("data") .. "/mason/bin"
+
 require("config.options")
 require("config.lazy")
 require("config.keymaps")
