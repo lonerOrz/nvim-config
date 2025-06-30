b0135f99ff02b1533ca47e04b0639d98167ac236
loner <2788892716@qq.com>
2025-07-01 01:31:42 +0800

    lang for lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -20,6 +20,7 @@ require("lazy").setup({
   spec = {
     -- import your plugins
     { import = "plugins" }, -- 导入`./lua/plugins/`目录下的所有lua文件
+    { import = "lang" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
@@ -29,7 +30,7 @@ require("lazy").setup({
   ui = {
     -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
     border = "rounded",
-  }
+  },
 })
 
 vim.keymap.set("n", "<leader>L", "<CMD>Lazy<CR>", { desc = "[Lazy] Open Lazy.nvim" })
---
+++ lua/lang/lua.lua
@@ -0,0 +1,46 @@
+-- vim.lsp.enable("lua_ls")
+
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		optional = true, -- 只有设置过nvim-treesitter才会加载
+		opts = {
+			ensure_installed = { "lua" },
+		},
+		opts_extend = { "ensure_installed" }, -- 以列表形式扩展opts的ensure_installed
+	},
+
+	{
+		"williamboman/mason.nvim",
+		optional = true,
+		opts = {
+			ensure_installed = {
+				"lua-language-server",
+				"stylua",
+			},
+		},
+		opts_extend = { "ensure_installed" },
+	},
+
+	{
+		"stevearc/conform.nvim",
+		optional = true,
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+			},
+		},
+	},
+
+	{
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+}
