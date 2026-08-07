93132e450b08057e259e72c24e17bb303fb2faad
loner <2788892716@qq.com>
2026-08-07 09:02:47 +0800

    feat: Find files on Home Dir

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,22 +1,28 @@
+-- lua/plugins/fzf.lua
 return {
 	"ibhagwan/fzf-lua",
 	event = "VeryLazy",
-	-- optional for icon support
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		require("fzf-lua").setup({
+		local fzf = require("fzf-lua")
+		fzf.setup({
 			winopts = {
-				height = 0.7, -- 窗口高度
-				width = 0.7, -- 窗口宽度
-				row = 0.5, -- 窗口位置
-				col = 0.5, -- 窗口位置
-				border = "rounded", -- 窗口边框样式
+				height = 0.85,
+				width = 0.80,
+				row = 0.5,
+				col = 0.5,
+				border = "rounded",
 			},
 		})
 
-		-- 设置 FZF 查找文件的快捷键
+		-- 项目根目录查找
 		vim.keymap.set("n", "<leader><space>", function()
-			require("fzf-lua").files({ cwd = vim.fn.getcwd() })
-		end, { desc = "[FZF] Find files (pwd)" })
+			fzf.files({ cwd = vim.fn.getcwd() })
+		end, { desc = "[FZF] Find files (Project Root)" })
+
+		-- 用户主目录查找
+		vim.keymap.set("n", "<leader>fa", function()
+			fzf.files({ cwd = "~" })
+		end, { desc = "[FZF] Find files (Home Dir)" })
 	end,
 }
